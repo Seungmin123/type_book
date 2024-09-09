222f9501c03c8480e390f652dd7da8ab5cf8f88a
Seungmin123 <dltmdals147@gmail.com>
2024-09-09 15:25:36 +0900

    init

---
+++ src/main/java/com/muzlive/kitpage/kit_page/KitPageApplication.java
@@ -0,0 +1,13 @@
+package com.muzlive.kitpage.kit_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muzlive/kitpage/kit_page/KitPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muzlive.kitpage.kit_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
