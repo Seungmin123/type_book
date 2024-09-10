32cf99f0a5380c7570b633556ad36d58c5a2d637
Seungmin82 <sm.lee@muzlive.com>
2024-09-10 11:51:49 +0900

    ADD - Dev 배포 프로세스 수정

--- src/main/java/com/muzlive/kitpage/kitpage/KitPageApplication.java
+++ src/main/java/com/muzlive/kitpage/kitpage/KitPageApplication.java
@@ -6,17 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class KitPageApplication {
 
-	public static String APPLICATION_LOCATIONS = "spring.config.location="
-		+ "classpath:application.yml,";
 	public static void main(String[] args) {
-		System.out.println("os.name===========" + System.getProperty("os.name"));
-		if(System.getProperty("os.name").indexOf("Windows") > -1)
-			APPLICATION_LOCATIONS += "optional:E:\\BackUp190425\\workspace_IntelliJ\\S3_config\\Global_SNS\\Tokyo\\application-prod_2.yml";
-		else if(System.getProperty("os.name").indexOf("Mac OS X") > -1){
-			APPLICATION_LOCATIONS += "optional:/Users/leeseumgin/Documents/source/fuji/application-local.yml";
-		}else {
-			APPLICATION_LOCATIONS += "optional:/kihno_data/WEB9/application-prod.yml";
-		}
+
+		String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "optional:/Users/leeseumgin/Documents/source/kitpage/application-local.yml,"
+			+ "optional:/source/application-dev.yml,"
+			+ "optional:/source/application-prod.yml,";
+
 		new SpringApplicationBuilder(KitPageApplication.class)
 			.properties(APPLICATION_LOCATIONS)
 			.run(args);
