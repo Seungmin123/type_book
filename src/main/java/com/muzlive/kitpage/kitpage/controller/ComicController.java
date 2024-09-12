8ac400a180d2a7ed69b5202f4667335d72521c23
Seungmin123 <dltmdals147@gmail.com>
2024-09-12 15:25:22 +0900

    ADD - TODO 작성

---
+++ src/main/java/com/muzlive/kitpage/kitpage/controller/ComicController.java
@@ -0,0 +1,30 @@
+package com.muzlive.kitpage.kitpage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/v1/comic")
+@RestController
+public class ComicController {
+
+	// TODO get ComicBook Info By contentId
+
+	// TODO get ComicBook Detail list By contentId
+
+	// TODO get ComicBook Detail By comicBookDetailUid
+
+	// TODO Insert BookMark
+
+	// TODO Delete BookMark
+
+	// TODO Image View APi -> ? -> Online -> image return
+
+	// TODO get Last View Location
+
+	// TODO get Music Info
+
+	// TODO get Video Info
+
+}
