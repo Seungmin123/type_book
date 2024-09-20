e97076b2b6411ff808359cb9c95e3ba3e8f2b258
Seungmin82 <sm.lee@muzlive.com>
2024-09-20 14:41:57 +0900

    FIX - Swagger

--- src/main/java/com/muzlive/kitpage/kitpage/config/filter/SwaggerFilter.java
+++ src/main/java/com/muzlive/kitpage/kitpage/config/filter/SwaggerFilter.java
@@ -1,5 +1,6 @@
 package com.muzlive.kitpage.kitpage.config.filter;
 
+import com.muzlive.kitpage.kitpage.utils.CommonUtils;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -16,32 +17,30 @@ public class SwaggerFilter extends OncePerRequestFilter {
     @Value("${security.access.iplist}")
     private String iplist;
 
-    // private final FujiUtils fujiUtils;
+    private final CommonUtils commonUtils;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        boolean result = true;
-//
-//        if (request.getRequestURI().contains("swagger")) {
-//            String[] splitAddress = iplist.split("[|]");
-//            String realIp = fujiUtils.getIp(request);
-//
-//            result = false;
-//
-//            for(String accessIP : splitAddress){
-//                if(accessIP.equals(realIp)){
-//                    result = true;
-//                }
-//            }
-//        }
-//
-//        if(result){
-//            filterChain.doFilter(request,response);
-//        }else {
-//            response.sendError(HttpServletResponse.SC_FORBIDDEN);
-//        }
+        boolean result = true;
 
-        filterChain.doFilter(request,response);
+        if (request.getRequestURI().contains("swagger")) {
+            String[] splitAddress = iplist.split("[|]");
+            String realIp = commonUtils.getIp(request);
+
+            result = false;
+
+            for(String accessIP : splitAddress){
+                if(accessIP.equals(realIp)){
+                    result = true;
+                }
+            }
+        }
+
+        if(result){
+            filterChain.doFilter(request,response);
+        }else {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
     }
 
 
--- src/main/java/com/muzlive/kitpage/kitpage/utils/CommonUtils.java
+++ src/main/java/com/muzlive/kitpage/kitpage/utils/CommonUtils.java
@@ -1,6 +1,7 @@
 package com.muzlive.kitpage.kitpage.utils;
 
 import java.util.Random;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,28 @@ public class CommonUtils {
 		int randomNum = rand.nextInt(256); // 0 ~ 255 사이의 랜덤 숫자 생성
 
 		return String.format("%02X", randomNum);
+	}
+
+	public String getIp(HttpServletRequest request) {
+		String ip = request.getHeader("X-Forwarded-For");
+
+		if(ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if(ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if(ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if(ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if(ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+
+		return ip;
 	}
 
 }
