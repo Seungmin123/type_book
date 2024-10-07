2128f2b460dfde6e4ede8b2d4c3b175e4fe0e2dc
Seungmin82 <sm.lee@muzlive.com>
2024-10-07 11:33:27 +0900

    FIX - 이미지 다운로드 API 수정

--- src/main/java/com/muzlive/kitpage/kitpage/utils/CommonUtils.java
+++ src/main/java/com/muzlive/kitpage/kitpage/utils/CommonUtils.java
@@ -1,5 +1,8 @@
 package com.muzlive.kitpage.kitpage.utils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -47,6 +50,17 @@ public class CommonUtils {
 		if(episode != null) name += " " + episode;
 
 		return name;
+	}
+
+	public byte[] inputStreamToByteArray(InputStream inputStream) throws IOException {
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		int nRead;
+		byte[] data = new byte[1024];
+		while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+			buffer.write(data, 0, nRead);
+		}
+		buffer.flush();
+		return buffer.toByteArray();
 	}
 
 }
