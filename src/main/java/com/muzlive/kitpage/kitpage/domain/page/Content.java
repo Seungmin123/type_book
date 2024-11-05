4fcb2c70b9049ac1611f02eee186ef241d702852
Seungmin82 <sm.lee@muzlive.com>
2024-11-05 15:52:25 +0900

    Fix - Order 지정

--- src/main/java/com/muzlive/kitpage/kitpage/domain/page/Content.java
+++ src/main/java/com/muzlive/kitpage/kitpage/domain/page/Content.java
@@ -13,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -55,6 +56,7 @@ public class Content extends BaseTimeEntity implements Serializable {
 	private Region region;
 
 	@OneToMany(mappedBy = "content")
+	@OrderBy("pageUid ASC")
 	private List<Page> pages;
 
 
--- src/main/java/com/muzlive/kitpage/kitpage/domain/page/repository/ContentRepository.java
+++ src/main/java/com/muzlive/kitpage/kitpage/domain/page/repository/ContentRepository.java
@@ -3,13 +3,13 @@ package com.muzlive.kitpage.kitpage.domain.page.repository;
 import com.muzlive.kitpage.kitpage.domain.page.Content;
 import com.muzlive.kitpage.kitpage.utils.enums.Region;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
-	@EntityGraph(attributePaths = {"pages"})
+	@Query("SELECT c FROM Content c LEFT JOIN FETCH c.pages p WHERE c.contentId = :contentId AND c.region = :region")
 	Optional<Content> findByContentIdAndRegion(String contentId, Region region);
 }
