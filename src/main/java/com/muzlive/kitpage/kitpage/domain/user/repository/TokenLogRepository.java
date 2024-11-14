07e9667557fa5bec33352224715051ae4f67702e
Seungmin82 <sm.lee@muzlive.com>
2024-11-14 13:51:21 +0900

    FIX - 토큰 유효성 검사 로직 수정

--- src/main/java/com/muzlive/kitpage/kitpage/domain/user/repository/TokenLogRepository.java
+++ src/main/java/com/muzlive/kitpage/kitpage/domain/user/repository/TokenLogRepository.java
@@ -1,10 +1,13 @@
 package com.muzlive.kitpage.kitpage.domain.user.repository;
 
 import com.muzlive.kitpage.kitpage.domain.user.TokenLog;
+import com.muzlive.kitpage.kitpage.utils.enums.TokenType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TokenLogRepository extends JpaRepository<TokenLog, Long> {
 
+	Optional<TokenLog> findFirstByDeviceIdAndTypeOrderByCreatedAtDesc(String deviceId, TokenType tokenType);
 }
