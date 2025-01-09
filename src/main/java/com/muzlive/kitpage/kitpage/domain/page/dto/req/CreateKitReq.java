7d4d4f76ab7caa4eee1eb460c239fb45e5e9ccd5
Seungmin82 <sm.lee@muzlive.com>
2025-01-09 17:38:08 +0900

    FIX - porting 생성자 추가

--- src/main/java/com/muzlive/kitpage/kitpage/domain/page/dto/req/CreateKitReq.java
+++ src/main/java/com/muzlive/kitpage/kitpage/domain/page/dto/req/CreateKitReq.java
@@ -3,9 +3,11 @@ package com.muzlive.kitpage.kitpage.domain.page.dto.req;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class CreateKitReq {
