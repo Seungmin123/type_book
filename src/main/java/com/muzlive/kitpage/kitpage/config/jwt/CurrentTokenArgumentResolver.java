cb98c21b8254fd636f46ef6ca2845d4b4d8c2620
Seungmin82 <sm.lee@muzlive.com>
2025-04-08 15:34:04 +0900

    REFACTORING - JWT 인증 컨텍스트 구성

---
+++ src/main/java/com/muzlive/kitpage/kitpage/config/jwt/CurrentTokenArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.muzlive.kitpage.kitpage.config.jwt;
+
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class CurrentTokenArgumentResolver implements HandlerMethodArgumentResolver {
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.hasParameterAnnotation(CurrentToken.class)
+			&& parameter.getParameterType().equals(String.class);
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+		return jwtTokenProvider.resolveToken(request);
+	}
+
+}
