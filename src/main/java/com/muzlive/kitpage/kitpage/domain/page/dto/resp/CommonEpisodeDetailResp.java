b87fd0ac43127d0d632aa46ee607713bd63bd3e0
Seungmin123 <dltmdals147@gmail.com>
2025-05-07 14:38:29 +0900

    ADD - Swagger oneOf 수정

--- src/main/java/com/muzlive/kitpage/kitpage/domain/page/dto/resp/CommonEpisodeDetailResp.java
+++ src/main/java/com/muzlive/kitpage/kitpage/domain/page/dto/resp/CommonEpisodeDetailResp.java
@@ -1,9 +1,21 @@
 package com.muzlive.kitpage.kitpage.domain.page.dto.resp;
 
+import com.muzlive.kitpage.kitpage.domain.page.comicbook.dto.resp.ComicBookImageResp;
+import com.muzlive.kitpage.kitpage.domain.page.photobook.dto.resp.PhotoBookEpisodeDetailResp;
+import com.muzlive.kitpage.kitpage.domain.page.photobook.dto.resp.PhotoBookImageResp;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(
+	description = "컨텐츠 상세 page 단위 응답 클래스",
+	oneOf = {
+		ComicBookImageResp.class,
+		PhotoBookImageResp.class,
+		PhotoBookEpisodeDetailResp.class
+	}
+)
 @AllArgsConstructor
 @Getter
 @Setter
