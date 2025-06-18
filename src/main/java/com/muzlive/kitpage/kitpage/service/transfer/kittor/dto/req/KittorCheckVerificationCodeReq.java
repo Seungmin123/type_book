60089ce83ade7c86b234d3a7a2d34fa99679314b
Seungmin123 <dltmdals147@gmail.com>
2025-06-18 11:20:01 +0900

    ADD - 인증번호 확인 API 추가

---
+++ src/main/java/com/muzlive/kitpage/kitpage/service/transfer/kittor/dto/req/KittorCheckVerificationCodeReq.java
@@ -0,0 +1,16 @@
+package com.muzlive.kitpage.kitpage.service.transfer.kittor.dto.req;
+
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KittorCheckVerificationCodeReq {
+
+	@NotNull
+	private String email;
+
+	@NotNull
+	private String verificationCode;
+}
